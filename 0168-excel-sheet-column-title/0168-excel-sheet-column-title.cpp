ce49d918044eb46e02b3e5af31261ea09ba043d4
ganesh khandekar <ganeshkhandekar092@gmail.com>
2026-07-10 09:44:33 +0530

    Time: 0 ms (100%), Space: 7.8 MB (25.13%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    string value(int num) {
+        switch (num) {
+        case 0:
+            return "Z";
+        case 1:
+            return "A";
+        case 2:
+            return "B";
+        case 3:
+            return "C";
+        case 4:
+            return "D";
+        case 5:
+            return "E";
+        case 6:
+            return "F";
+        case 7:
+            return "G";
+        case 8:
+            return "H";
+        case 9:
+            return "I";
+        case 10:
+            return "J";
+        case 11:
+            return "K";
+        case 12:
+            return "L";
+        case 13:
+            return "M";
+        case 14:
+            return "N";
+        case 15:
+            return "O";
+        case 16:
+            return "P";
+        case 17:
+            return "Q";
+        case 18:
+            return "R";
+        case 19:
+            return "S";
+        case 20:
+            return "T";
+        case 21:
+            return "U";
+        case 22:
+            return "V";
+        case 23:
+            return "W";
+        case 24:
+            return "X";
+        case 25:
+            return "Y";
+        }
+        return " ";
+    }
+    string convertToTitle(int columnNumber, string st = "") {
+
+        while (columnNumber > 0) {
+
+            st = value(columnNumber % 26) + st;
+            if (columnNumber % 26 == 0) {
+                columnNumber--;
+            }
+            columnNumber = columnNumber / 26;
+        }
+
+        return st;
+    }
+};
