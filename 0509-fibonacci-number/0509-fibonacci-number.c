9a24596a39bcb493bf430634ebc7a552bb065df2
ganesh khandekar <ganeshkhandekar092@gmail.com>
2026-07-10 09:48:20 +0530

    Time: 2 ms (73%), Space: 7.7 MB (100%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.c
@@ -0,0 +1,20 @@
+
+
+int fib(int n){
+
+
+    int f[100];
+    f[0]=0;
+    f[1]=1;
+    
+   
+
+    for(int i=2; i<=n;i++)
+    {
+        f[i]=f[i-1]+f[i-2];
+        
+    }
+    
+return f[n];
+
+ }
