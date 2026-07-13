0a7532d5d4204781b5bb21ac4a89431a55c56854
ganesh khandekar <ganeshkhandekar092@gmail.com>
2026-07-13 12:47:56 +0530

    Time: 45 ms (5.66%), Space: 44.4 MB (5.23%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,5 +1,14 @@
 class Solution {
     public int reverseBits(int n) {
-        return Integer.reverse(n);
+        int res=0;
+        for(int i=1;i<32;i++){
+            if(n%2==1){
+                res++;
+            }
+            n=n>>1;
+            res=res<<1;
+            System.out.println(i+"res "+res+" n "+n);
+        }
+        return res;
     }
 }
