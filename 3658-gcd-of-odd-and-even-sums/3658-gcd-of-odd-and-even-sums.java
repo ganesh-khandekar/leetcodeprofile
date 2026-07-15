7e4bbbd8106da27e34d1fb7c2e18046175e9a01a
ganesh khandekar <ganeshkhandekar092@gmail.com>
2026-07-15 15:57:40 +0530

    Time: 1 ms (83.11%), Space: 42.9 MB (6.12%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,5 +1,10 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
         return n;
+        /* reason
+        -> sumOdd = 1+3+⋯+(2n−1) = n^2 
+        -> sumEven = 2+4+⋯+2n = n(n+1) 
+        -> GCD is comman of both which is n 
+        -> so, I return n */ 
     }
 }
