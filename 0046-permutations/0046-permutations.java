3a28a5e4b18c35a13e3ecad766ad9ddb7ee6549e
ganesh khandekar <ganeshkhandekar092@gmail.com>
2026-07-29 14:08:21 +0530

    Time: 2 ms (33.88%), Space: 45.5 MB (62.87%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,20 +1,17 @@
 class Solution {
     List<List<Integer>> res =new ArrayList<>();
-    List<Integer> index =new ArrayList<>();
     void travel(int[] nums ,List<Integer> arr,int k){
         if(arr.size()==nums.length){
             res.add(new ArrayList(arr));
             return;
         }
         for(int i=k;i<nums.length;i++){
-            if(index.contains(i)){
+            if(arr.contains(nums[i])){
                 continue;
             }
             arr.add(nums[i]);
-            index.add(i);
             travel(nums,arr,0);
             arr.removeLast();
-            index.removeLast();
         }
     }
     public List<List<Integer>> permute(int[] nums) {
